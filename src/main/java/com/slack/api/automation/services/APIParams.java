13749ce5c549851860cee64ea5fa9f8bf632b25a
Ravi Beck <ravi.beck@coviam.com>
2020-04-18 21:03:45 +0530

    Update APIParams.java

--- src/main/java/com/slack/api/automation/services/APIParams.java
+++ src/main/java/com/slack/api/automation/services/APIParams.java
@@ -1,5 +1,6 @@
 package com.slack.api.automation.services;
 
+import com.slack.api.automation.utilities.PropertyFileReader;
 import io.restassured.RestAssured;
 import io.restassured.specification.RequestSpecification;
 
@@ -7,6 +8,6 @@ class APIParams {
     static RequestSpecification setParams() {
         return RestAssured.given()
                 .param("Content-type", "application/x-www-form-urlencoded")
-                .param("token","xoxp-1063335876244-1063335876292-1056308849475-f3171137b585d12e58e201040821cb4e");
+                .param("token", PropertyFileReader.getTOKEN());
     }
 }
